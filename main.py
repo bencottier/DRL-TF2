c782bd3f47d4a0078170eea86618a6230688577c
Ben Cottier <bcottier2@gmail.com>
2019-08-24 21:41:13 +1000

    Update main with logger args

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ author: Ben Cottier (git: bencottier)
 """
 from __future__ import absolute_import, division, print_function, unicode_literals
 from ddpg import ddpg
+from utils import setup_logger_kwargs
 import gym
 import argparse
 
@@ -17,12 +18,15 @@ if __name__ == '__main__':
     parser.add_argument('--arch', type=str, default='mlp')
     parser.add_argument('--hid', type=int, default=300)
     parser.add_argument('--l', type=int, default=1)
-    parser.add_argument('--gamma', type=float, default=0.99)
+    parser.add_argument('--discount', type=float, default=0.99)
     parser.add_argument('--seed', '-s', type=int, default=0)
     parser.add_argument('--epochs', type=int, default=50)
     parser.add_argument('--exp_name', type=str, default='ddpg')
     args = parser.parse_args()
 
+    logger_kwargs = setup_logger_kwargs(args.exp_name, args.seed)
+
     ddpg(lambda : gym.make(args.env), ac_arch=args.arch,
          ac_kwargs=dict(hidden_sizes=[args.hid]*args.l),
-         gamma=args.gamma, seed=args.seed, epochs=args.epochs)
+         discount=args.discount, seed=args.seed, epochs=args.epochs,
+         logger_kwargs=logger_kwargs)
--- utils.py
+++ utils.py
@@ -6,10 +6,32 @@ Utility functions.
 
 author: Ben Cottier (git: bencottier)
 """
+import time
+import os.path as osp
 
-def get_vars(scope):
-    return [x for x in tf.global_variables() if scope in x.name]
 
-def count_vars(scope):
-    v = get_vars(scope)
-    return sum([np.prod(var.shape.as_list()) for var in v])
+FORCE_DATESTAMP = False
+DEFAULT_DATA_DIR = './out'
+
+
+def setup_logger_kwargs(exp_name, seed=None, data_dir=None, datestamp=False):
+    # Datestamp forcing
+    datestamp = datestamp or FORCE_DATESTAMP
+
+    # Make base path
+    ymd_time = time.strftime("%Y-%m-%d_") if datestamp else ''
+    relpath = ''.join([ymd_time, exp_name])
+    
+    if seed is not None:
+        # Make a seed-specific subfolder in the experiment directory.
+        if datestamp:
+            hms_time = time.strftime("%Y-%m-%d_%H-%M-%S")
+            subfolder = ''.join([hms_time, '-', exp_name, '_s', str(seed)])
+        else:
+            subfolder = ''.join([exp_name, '_s', str(seed)])
+        relpath = osp.join(relpath, subfolder)
+
+    data_dir = data_dir or DEFAULT_DATA_DIR
+    logger_kwargs = dict(output_dir=osp.join(data_dir, relpath), 
+                         exp_name=exp_name)
+    return logger_kwargs
