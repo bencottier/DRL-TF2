2b78e845280a29f45924934bf9d6574fcc230852
Ben Cottier <bcottier2@gmail.com>
2019-08-24 21:41:01 +1000

    Add main loop to DDPG

--- ddpg.py
+++ ddpg.py
@@ -12,40 +12,159 @@ from actor_critic import Actor, Critic
 from replay_buffer import ReplayBuffer
 import tensorflow as tf
 import numpy as np
-
-
-def update_target(target, current, polyak=0.995):
-    target.set_weights(polyak * target.get_weights() + (1 - polyak) * current.get_weights())
+import time
+import os
     
 
 def ddpg(env_fn, ac_arch=MLP, ac_kwargs=dict(), seed=0, 
-         steps_per_epoch=5000, epochs=100, replay_size=int(1e6), gamma=0.99, 
+         steps_per_epoch=5000, epochs=100, replay_size=int(1e6), discount=0.99, 
          polyak=0.995, pi_lr=1e-3, q_lr=1e-3, batch_size=100, start_steps=10000, 
-         act_noise=0.1, max_ep_len=1000, save_freq=1):
-    # TODO: logging
+         act_noise=0.1, max_ep_len=1000, logger_kwargs=dict(), save_freq=1):
+    
+    # TODO: EpochLogger class
+    logger = EpochLogger(**logger_kwargs)
+    logger.save_config(locals())
+
     # Set random seed for relevant modules
     tf.random.set_seed(seed)
     np.random.seed(seed)
+
     # Create environment
     env, test_env = env_fn(), env_fn()
     obs_dim = env.observation_space.shape[0]
     act_dim = env.action_space.shape[0]
+
     # Action limit for clamping: critically, assumes all dimensions share the same bound!
     act_limit = env.action_space.high[0]
+
     # Share information about action space with policy architecture
     ac_kwargs['action_space'] = env.action_space
+
     # Randomly initialise critic and actor networks
-    critic = Critic(input_shape=(batch_size, obs_dim + act_dim), **ac_kwargs)
+    critic = Critic(discount, input_shape=(batch_size, obs_dim + act_dim), **ac_kwargs)
     actor = Actor(input_shape=(batch_size, obs_dim), **ac_kwargs)
+
     # Initialise target networks
     critic_target = Critic(input_shape=(batch_size, obs_dim + act_dim), **ac_kwargs)
     actor_target = Actor(input_shape=(batch_size, obs_dim), **ac_kwargs)
     critic_target.set_weights(critic.get_weights())
     actor_target.set_weights(actor.get_weights())
+
     # Initialise replay buffer
-    replay_buffer = ReplayBuffer(size=replay_size)
+    replay_buffer = ReplayBuffer(obs_dim, act_dim, size=replay_size)
 
+    # Optimizers
+    critic_optimizer = tf.keras.optimizers.Adam(q_lr)
+    actor_optimizer = tf.keras.optimizers.Adam(pi_lr)
 
-    # update_target(critic_target, critic, polyak)
-    # update_target(actor_target, actor, polyak)
+    # Set up checkpointing
+    checkpoint_dir = './out/training_checkpoints'
+    checkpoint_prefix = os.path.join(checkpoint_dir, "ckpt")
+    checkpoint = tf.train.Checkpoint(critic_optimizer=critic_optimizer,
+                                    actor_optimizer=actor_optimizer,
+                                    critic=critic,
+                                    actor=actor)
 
+    def get_action(o, noise_scale):
+        a = Actor(o.reshape(1, -1))
+        a += noise_scale * np.random.randn(act_dim)
+        return np.clip(a, -act_limit, act_limit)
+
+    def test_agent(n=10):
+        for _ in range(n):
+            o, r, d, ep_ret, ep_len = test_env.reset(), 0, False, 0, 0
+            while not(d or (ep_len == max_ep_len)):
+                # Take deterministic actions at test time (noise_scale=0)
+                o, r, d, _ = test_env.step(get_action(o, 0))
+                ep_ret += r
+                ep_len += 1
+            logger.store(TestEpRet=ep_ret, TestEpLen=ep_len)
+
+    start_time = time.time()
+    o, r, d, ep_ret, ep_len = env.reset(), 0, False, 0, 0
+    total_steps = steps_per_epoch * epochs
+
+    for t in range(total_steps):
+        """
+        Until start_steps have elapsed, randomly sample actions
+        from a uniform distribution for better exploration. Afterwards, 
+        use the learned policy (with some noise, via act_noise). 
+        """
+        if t > start_steps:
+            a = get_action(o, act_noise)
+        else:
+            a = env.action_space.sample()
+
+        # Step the env
+        o2, r, d, _ = env.step(a)
+        ep_ret += r
+        ep_len += 1
+        
+        # Ignore the "done" signal if it comes from hitting the time
+        # horizon (that is, when it's an artificial terminal signal
+        # that isn't based on the agent's state)
+        d = False if ep_len==max_ep_len else d
+
+        # Store experience in replay buffer
+        replay_buffer.store(o, a, r, o2, d)
+
+        # Advance the stored state
+        o = o2
+
+        if d or (ep_len == max_ep_len):
+            """
+            Perform all DDPG updates at the end of the trajectory,
+            in accordance with tuning done by TD3 paper authors.
+            """
+            for _ in range(ep_len):
+                batch = replay_buffer.sample_batch(batch_size)
+
+                # Q-learning update
+                with tf.GradientTape() as critic_tape:
+                    q = critic(batch['obs1'], batch['acts'])
+                    q_pi_targ = critic_target(batch['obs2'], actor_target(batch['obs2']))
+                    backup = critic_target.bellman_backup(batch['rwds'], batch['done'], q_pi_targ)
+                    q_loss = critic.loss(q, backup)
+                critic_gradients = critic_tape.gradient(q_loss, critic.trainable_variables)
+                critic_optimizer.apply_gradients(zip(critic_gradients, critic.trainable_variables))
+                logger.store(LossQ=q_loss, QVals=q)
+
+                # Policy update
+                with tf.GradientTape() as actor_tape:
+                    pi = actor(batch['obs1'])
+                    q_pi = critic(batch['obs1'], pi)
+                    pi_loss = actor.loss(q_pi)
+                actor_gradients = actor_tape.gradient(pi_loss, actor.trainable_variables)
+                actor_optimizer.apply_gradients(zip(actor_gradients, actor.trainable_variables))
+                logger.store(LossPi=pi_loss)
+
+                # Target update
+                critic_target.polyak_update(critic, polyak)
+                actor_target.polyak_update(actor, polyak)
+
+            logger.store(EpRet=ep_ret, EpLen=ep_len)
+            o, r, d, ep_ret, ep_len = env.reset(), 0, False, 0, 0
+
+        # End of epoch wrap-up
+        if t > 0 and t % steps_per_epoch == 0:
+            epoch = t // steps_per_epoch
+
+            # Save model
+            if (epoch % save_freq == 0) or (epoch == epochs - 1):
+                checkpoint.save(file_prefix=checkpoint_prefix)
+
+            # Test the performance of the deterministic version of the agent.
+            test_agent()
+
+            # Log info about epoch
+            logger.log_tabular('Epoch', epoch)
+            logger.log_tabular('EpRet', with_min_and_max=True)
+            logger.log_tabular('TestEpRet', with_min_and_max=True)
+            logger.log_tabular('EpLen', average_only=True)
+            logger.log_tabular('TestEpLen', average_only=True)
+            logger.log_tabular('TotalEnvInteracts', t)
+            logger.log_tabular('QVals', with_min_and_max=True)
+            logger.log_tabular('LossPi', average_only=True)
+            logger.log_tabular('LossQ', average_only=True)
+            logger.log_tabular('Time', time.time() - start_time)
+            logger.dump_tabular()
