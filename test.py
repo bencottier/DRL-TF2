dc71fab7673632eb1ce56f79c27d608a0f0894e1
Ben Cottier <bcottier2@gmail.com>
2019-09-14 18:44:46 +0100

    Add checkpoint arg and help messages to test.py

--- test.py
+++ test.py
@@ -16,7 +16,7 @@ import argparse
 import os
 
 
-def test_policy(output_dir, env_name, episodes=10):
+def test_policy(output_dir, env_name, episodes, checkpoint_number):
     """
     Run a learned policy with visualisation in the environment.
 
@@ -32,8 +32,7 @@ def test_policy(output_dir, env_name, episodes=10):
         exp_data = json.load(f)
 
     # Create environment
-    env_fn = lambda : gym.make(env_name)
-    env, test_env = env_fn(), env_fn()
+    env = gym.make(env_name)
     obs_dim = env.observation_space.shape[0]
     act_dim = env.action_space.shape[0]
 
@@ -54,30 +53,38 @@ def test_policy(output_dir, env_name, episodes=10):
                                     actor_optimizer=actor_optimizer,
                                     critic=critic,
                                     actor=actor)
-    # checkpoint.restore(os.path.join(checkpoint_dir, 'ckpt-1')).expect_partial()
-    checkpoint.restore(tf.train.latest_checkpoint(checkpoint_dir)).expect_partial()
+    if checkpoint_number is not None:
+        checkpoint.restore(os.path.join(checkpoint_dir, f'ckpt-{checkpoint_number}')).expect_partial()
+    else:
+        checkpoint.restore(tf.train.latest_checkpoint(checkpoint_dir)).expect_partial()
 
     # Run policy for specified number of episodes, recording return
     ep_rets = np.zeros(episodes)
     for i in range(episodes):
-        o, r, d, ep_ret, ep_len = test_env.reset(), 0, False, 0, 0
+        o, r, d, ep_ret, ep_len = env.reset(), 0, False, 0, 0
         while not(d or (ep_len == exp_data['max_ep_len'])):
-            test_env.render()
-            o, r, d, _ = test_env.step(actor(o.reshape(1, -1)))
+            env.render()
+            o, r, d, _ = env.step(actor(o.reshape(1, -1)))
             ep_ret += r
             ep_len += 1
         ep_rets[i] = ep_ret
-        print(f'Episode {i}: return={ep_ret:.0f} length={ep_len}')
+        print(f'Episode {i}: return={ep_ret[0]:.0f} length={ep_len}')
     # Summary stats
     print(f'avg={ep_rets.mean():.0f} std={ep_rets.std():.0f} ' \
             f'min={ep_rets.min():.0f} max={ep_rets.max():.0f}')
+    env.close()
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--output_dir', type=str, default='./out/ddpg-benchmark-cheetah-s0')
-    parser.add_argument('--env_name', type=str, default='HalfCheetah-v2')
-    parser.add_argument('--episodes', type=int, default=10)
+    parser.add_argument('--output_dir', type=str, default='./out/demo',
+            help='Directory containing config.json and training_checkpoints folder')
+    parser.add_argument('--env_name', type=str, default='Pendulum-v0',
+            help='Environment name, normally matches training')
+    parser.add_argument('--episodes', type=int, default=100,
+            help='Number of episodes to run')
+    parser.add_argument('--checkpoint_number', type=int, default=None,
+            help='Checkpoint to load models from (default latest)')
     args = parser.parse_args()
 
-    test_policy(args.output_dir, args.env_name, args.episodes)
+    test_policy(args.output_dir, args.env_name, args.episodes, args.checkpoint_number)
