1c8e179be590f8562b0e06d24eebfa054ce48b3f
Ben Cottier <bcottier2@gmail.com>
2019-11-09 22:11:24 +0000

    Add TD3 to training options

--- train.py
+++ train.py
@@ -7,7 +7,7 @@ Main program.
 author: Ben Cottier (git: bencottier)
 """
 from __future__ import absolute_import, division, print_function, unicode_literals
-import ddpg
+import ddpg, td3
 from utils import setup_logger_kwargs
 import gym
 import argparse
@@ -33,7 +33,7 @@ if __name__ == '__main__':
             help='name for this experiment, used for the logging folder')
     args = parser.parse_args()
 
-    algos = dict(ddpg=ddpg.run)
+    algos = dict(ddpg=ddpg.run, td3=td3.run)
 
     logger_kwargs = setup_logger_kwargs(args.exp_name, args.seed, datestamp=False)
     algos[args.algo.lower()](lambda : gym.make(args.env), logger_kwargs, args)
