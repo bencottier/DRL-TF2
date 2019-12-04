80dd547f3d7d8f79ff32569f8d38ed72bf1354dd
Ben Cottier <bcottier2@gmail.com>
2019-12-04 23:23:09 +0000

    Restore default ep_len in td3

--- td3.py
+++ td3.py
@@ -22,7 +22,7 @@ import os
 def td3(env_fn, ac_kwargs=dict(), seed=0, steps_per_epoch=5000, epochs=100, 
         replay_size=int(1e6), discount=0.99, polyak=0.995, pi_lr=1e-3, q_lr=1e-3, 
         batch_size=100, start_steps=10000, act_noise=0.1, target_noise=0.2,
-        noise_clip=0.5, policy_delay=2, num_q=2, max_ep_len=157, 
+        noise_clip=0.5, policy_delay=2, num_q=2, max_ep_len=1000, 
         logger_kwargs=dict(), save_freq=1):
     """
     Implements the deep deterministic policy gradient algorithm.
