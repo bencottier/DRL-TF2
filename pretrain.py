ceb76ab16d6988c2ac9777ef8974a6d6319fe714
Ben Cottier <bcottier2@gmail.com>
2019-12-07 11:28:18 +0000

    Add state dataset generator

--- pretrain.py
+++ pretrain.py
@@ -10,16 +10,52 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 from replay_buffer import ReplayBuffer
 from logger import EpochLogger
 from autoencoder import ConvolutionalAutoencoder
-from utils import scale_float, scale_uint8
+from utils import convert_json, scale_float, scale_uint8
 import tensorflow as tf
 import numpy as np
+import gym
+import tqdm
+import json
 import PIL
 import time
 import os
 
 
-def generate_state_dataset(env_name, size=int(1e5), max_ep_len=1000):
-    pass
+def generate_state_dataset(env_name, save_path, resume_from=0, 
+    im_size=(128, 128), num_samples=int(1e5), max_ep_len=1000):
+
+    if resume_from <= 0:
+        output = json.dumps(convert_json(locals()), separators=(',',':\t'), indent=4, 
+            sort_keys=True)
+        save_path = os.path.join(save_path, env_name)
+        os.makedirs(save_path, exist_ok=True)
+        with open(os.path.join(save_path, "config.json"), 'w') as out:
+            out.write(output)
+    else:
+        print(f'Resuming from sample {resume_from}')
+
+    print(f'State dataset of {num_samples:.0e} samples for {env_name} '
+        f'saved to {save_path}')
+
+    env = gym.make(env_name)
+    sample = resume_from
+    with tqdm.tqdm(total=num_samples) as pbar:  # create a progress bar
+        while sample < num_samples:
+            _, d, ep_len = env.reset(), False, 0
+            while not (d or (ep_len == max_ep_len)):
+                # Save the frame as a downsampled RGB JPEG
+                PIL.Image.fromarray(env.render(mode='rgb_array')).\
+                    resize(im_size, resample=PIL.Image.BILINEAR).\
+                    save(os.path.join(save_path, f'frame{sample}.jpg'), "JPEG")
+
+                a = env.action_space.sample()
+                _, _, d, _ = env.step(a)
+                ep_len += 1
+                sample += 1
+                pbar.update(1)
+
+                if sample >= num_samples:
+                    break
 
 
 def train_state_encoding(env_name, model_kwargs=dict(), seed=0, 
@@ -37,6 +73,7 @@ def train_state_encoding(env_name, model_kwargs=dict(), seed=0,
     np.random.seed(seed)
 
     # Create environment
+    env_fn = lambda: gym.make(env_name)
     env, test_env = env_fn(), env_fn()
     obs_dim = env.observation_space.shape[0]
     act_dim = env.action_space.shape[0]
@@ -44,13 +81,13 @@ def train_state_encoding(env_name, model_kwargs=dict(), seed=0,
     # Initialise model used for encoding
     autoencoder = ConvolutionalAutoencoder(**model_kwargs)
 
-    # Initialise replay buffer for storing and getting batches of transitions
-    replay_buffer = ReplayBuffer(obs_dim, act_dim, size=replay_size)
+    # Initialise dataset
+    # TODO
 
     # Set up model checkpointing so we can resume training or test separately
     checkpoint_dir = os.path.join(logger.output_dir, 'training_checkpoints')
     checkpoint_prefix = os.path.join(checkpoint_dir, "ckpt")
-    model_dict = {f'state_autoencoder{i}': autoencoder}
+    model_dict = {f'state_autoencoder': autoencoder}
     checkpoint = tf.train.Checkpoint(**model_dict)
 
     # @tf.function
@@ -63,12 +100,8 @@ def train_state_encoding(env_name, model_kwargs=dict(), seed=0,
             zip(gradients, autoencoder.trainable_variables))
         return loss
 
-    
 
-
-if __name__ == '__main__':
-    import gym
-
+def test_pipeline():
     autoencoder = ConvolutionalAutoencoder([64, 64, 64, 1], 3)
 
     env_name = 'Hopper-v2'
@@ -97,3 +130,9 @@ if __name__ == '__main__':
     plt.figure()
     plt.imshow(im_out[0])
     plt.show()
+
+
+if __name__ == '__main__':
+    # test_pipeline()
+
+    generate_state_dataset('Hopper-v2', './data/state')
